c4fb72b0fe2f958d137f739b72a2079bf6b01e48
DESKTOP-1KM32GD\edz <773815862@qq.com>
2018-10-15 14:08:07 +0800

    测试环境

---
+++ test/public/js/public.js
@@ -0,0 +1,18 @@
+﻿//监控 代码
+
+var monitor = {
+    ajaxJump:function (url, type, ApITitle) {
+       let mid = getCookie('mid');
+       var MobileNum = getCookie('userTel');
+       let curl = url;
+       let result2 = `MobileNum=${MobileNum}&MID=${mid}&Url=${curl}&Type2=${type}`;
+       $.ajax({
+           type: 'post',
+           url: ApITitle + "api/Regist_Login/Ins_User",
+           data: result2,
+           success: function () {
+               window.location.href = `${url}`;
+           }
+       })
+    }
+}
