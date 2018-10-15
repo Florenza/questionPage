5ab3c3b90fe61c6436cb753431e98c07e512c459
DESKTOP-1KM32GD\edz <773815862@qq.com>
2018-10-15 14:24:07 +0800

    测试更新提交

---
+++ test/public/js/baiduMonitor.js
@@ -0,0 +1,7 @@
+﻿var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?49a13640af24bd7055d9c12f90771907";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
