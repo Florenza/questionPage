a044896c198700ac24dc72a5be0ed37792156db0
DESKTOP-1KM32GD\edz <773815862@qq.com>
2018-10-15 15:14:43 +0800

    新测试环境

---
+++ testEnvironment/questionPage/loading/js/loading.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    let jumpUrl = "http://www.baidu.com";
+    let curl = "questionPage2_3.html"
+    let url = getUrlParam("url");
+    url = unescape(url);
+
+    if (url != "" && url != null && url != undefined) {
+        jumpUrl = url;
+       // alert(url);
+    }
+    else {
+       window.location.href = curl;
+    }
+    var time = 3
+    var countDown = setInterval(function () {
+        time--;
+        console.log(time);
+        if (time > 0) {
+            $(".loading_text span").text(time);
+            $(".main .option .blue_text span").text(time)
+        }
+        else {
+            window.location.href = jumpUrl;
+        }
+    }, 1000)
+})
---
+++ testEnvironment/questionPage/questionPage2_2/js/questionPage2_2.js
@@ -0,0 +1,207 @@
+﻿"use strict";
+
+var refUrl = "questionPage2_1.html";
+
+var jumpUrl = "questionPage2_3.html";
+
+var APITitle = "http://mes.jubao360.cn/"; //http://123.56.229.241:8003/http://mes.jubao360.cn/"http://localhost:54087/
+
+
+var APIUrl = APITitle + "api/Questionnaire/Post2";
+$(function () {
+
+    var AB_ID = getUrlParam("AB_ID");
+
+    var loanTime = getUrlParam("loanTime");
+
+    var login = getCookie("login");
+
+    var limit = getUrlParam("limit");
+
+    var money = void 0,
+        workTime = void 0,
+        jobType = void 0,
+        revenue = void 0,
+        qualifications=void 0,
+        ID = void 0;
+    var userTel = getCookie("userTel");
+    var backAPIUrl = APITitle + "api/Questionnaire/CheckAnswerByQNID/2?mobileNum=" + userTel;
+
+
+    subPage.checkHasAB_ID(login,userTel, AB_ID);
+    $.ajax({
+        type: 'get',
+        url: backAPIUrl,
+        data: {},
+        success: function success(data) {
+            console.log(data);
+            var result = void 0;
+            for (var i = 0; i < data.length; i++) {
+                subPage.isNumBack(data[i], 13);
+            }
+            console.log(money, workTime, jobType, revenue, ID);
+        }
+
+    });
+
+    var backAPIUrl = APITitle + "api/Questionnaire/CheckAnswerByQNID/2?mobileNum=" + userTel;
+
+    var nowCity = "";
+
+    console.log($("#hasWLD").children());
+
+    var hWLD = $("#hasWLD").find(".tab span").hasClass("decide_blue");
+
+    $("#back").click(function () {
+        window.location.href = "questionPage2_1.html?AB_ID=" + AB_ID;
+    });
+
+    $(".radioBtn .tab span").on('click', function () {
+        subPage.btnCss(this);
+    });
+    $(".submit .y_btn").click(function () {
+
+        var hCard = $("#hasCard").find(".tab span").hasClass("decide_blue");
+        var hasHorse = $("#hasHorse").find(".tab span").hasClass("decide_blue");
+        var hasCar = $("#hasCar").find(".tab span").hasClass("decide_blue");
+        var hasBaodan = $("#hasBaodan").find(".tab span").hasClass("decide_blue");
+        var hasShebao = $("#hasShebao").find(".tab span").hasClass("decide_blue");
+        var hasGongjijin = $("#hasGongjijin").find(".tab span").hasClass("decide_blue");
+
+        if (!hCard) {
+            alert("请填写是否有信用卡");
+            return;
+        } else if (!hasHorse) {
+            alert("请填写是否有房产");
+            return;
+        } else if (!hasCar) {
+            alert("请填写是否有车");
+            return;
+        } else if (!hasBaodan) {
+            alert("请填写是否有保单");
+            return;
+        } else if (!hasShebao) {
+            alert("请填写是否有社保");
+            return;
+        } else if (!hasGongjijin) {
+            alert("请填写是否有公积金");
+            return;
+        }
+        $(".y_btn").hide();
+        $(".load-3").show();
+        var applyResult = $(".decide_blue").text().split('');
+        var applyResultArry = subPage.replaceString(applyResult);
+        JXmorie(APITitle, '24');
+        $.ajax({
+            type: 'post',
+            url: APIUrl,
+            data: {
+                QNID: 2,
+                AB_ID: AB_ID,
+                MobileNum: userTel,
+                QID_13: "" + applyResultArry[0] + "",
+
+                QID_14: "" + applyResultArry[1] + "",
+
+                QID_15: "" + applyResultArry[2] + "",
+
+                QID_16: "" + applyResultArry[3] + "",
+
+                QID_17: "" + applyResultArry[4] + "",
+
+                QID_18: "" + applyResultArry[5] + "",
+
+                QID_19: "" + applyResultArry[6] + ""
+
+            },
+            success: function success(data) {
+                console.log(data);
+                console.log(AB_ID);
+                window.location.href = jumpUrl + "?AB_ID=" + AB_ID;
+                $(".y_btn").show();
+                $(".load-3").hide();
+            }
+        });
+
+    });
+});
+
+function JXmorie(APITitle, type2) {
+    var cul = getCookie("curl");
+
+    var preurl = getCookie("preurl");
+
+    var mid = getCookie("mid");
+
+    var Url = window.location.href;
+
+    var tel = getCookie("userTel");
+    var cookiesNum = getCookie("cookiesNum");
+
+    var result2 = "MobileNum=" + tel + "&cul=" + cul + "&MID=" + mid + "&Url=" + Url + "&Type2=" + type2 + "&preurl=" + preurl + "&CookiesNum=" + cookiesNum;
+
+    $.ajax({
+        data: result2,
+        type: 'post',
+        url: APITitle + "api/Regist_Login/Ins_User",
+        success: function success(data) {
+            console.log("JX", data);
+        }
+    });
+}
+var subPage = {
+    checkHasAB_ID: function checkHasAB_ID(login,userTel, checkID) {
+        if (checkID == "" || checkID == null || checkID == undefined || userTel == "" || userTel == undefined || userTel == null) {
+            window.location.href = refUrl;
+        }
+        else {
+            if (login != 1) {//判断是否有登录状态
+                login = 1;//补录登录状态
+                setCookie("login", login);
+            }
+        }
+    },
+    btnCss: function btnCss(e) {
+        $(e).addClass("decide_blue").siblings().removeClass("decide_blue");
+        var decideValue = $(e).attr("data-id");
+        $("#radioBtnValue").val(decideValue);
+    },
+    replaceString: function replaceString(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].replace("是", "y");
+            arr[i] = arr[i].replace("否", "n");
+        }
+        return arr;
+    },
+    isNumBack: function isNumBack(data, i) {
+
+        if (data.QID == i) {
+            this.backResult(data.QID, data.Value);
+        } else if (data.QID == i + 1) {
+            this.backResult(data.QID, data.Value);
+        } else if (data.QID == i + 2) {
+            this.backResult(data.QID, data.Value);
+        } else if (data.QID == i + 3) {
+            this.backResult(data.QID, data.Value);
+        } else if (data.QID == i + 4) {
+            this.backResult(data.QID, data.Value);
+        } else if (data.QID == i + 5) {
+            this.backResult(data.QID, data.Value);
+        } else if (data.QID == i + 6) {
+            this.backResult(data.QID, data.Value);
+        }
+    },
+    backResult: function backResult(i, dataValue) {
+
+        
+        var id = i - 12;
+        var isDecided=$(".content .content_tr").eq(id).find(".tab span").hasClass("decide_blue");
+        if (!isDecided) {
+            if (dataValue == "y") {
+                $(".content .content_tr").eq(id).find(".tab span").eq(0).addClass("decide_blue");
+            } else {
+                $(".content .content_tr").eq(id).find(".tab span").eq(1).addClass("decide_blue");
+            }
+        }
+    }
+};
