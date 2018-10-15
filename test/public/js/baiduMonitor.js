a6b28b24ef385dcb65bc4159f1839a3640717e84
DESKTOP-1KM32GD\edz <773815862@qq.com>
2018-10-15 14:20:49 +0800

    Revert "测试环境"
    This reverts commit c4fb72b0fe2f958d137f739b72a2079bf6b01e48.

--- test/public/js/baiduMonitor.js
+++
@@ -1,7 +0,0 @@
-﻿var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?49a13640af24bd7055d9c12f90771907";
-    var s = document.getElementsByTagName("script")[0]; 
-    s.parentNode.insertBefore(hm, s);
-})();
